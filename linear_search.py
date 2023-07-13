0f90a21997cf76d78d65df31ef219bcf79fe8336
Deboneil <bhattacharjeedeboneil@gmail.com>
2023-07-13 21:35:51 +0530

    first

---
+++ linear_search.py
@@ -0,0 +1,9 @@
+def linear_search(list, target):
+    for i in range(len(list)):
+        if list[i] == target:
+            print(f"found!! at {list[i]} ")
+            break
+
+list = [1,2,3,4,5,6]
+target = 3
+linear_search(list, target)
